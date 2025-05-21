530c643f73f8fd09d21735c2b541e53f76c70f5f
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-21 19:47:01 +0300

    Lesson 7 - Default Function parameters.

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,7 @@
-const pizzas = [
-    { name: 'Pepperoni', toppings: ['pepperoni']}
-];
+function multiply(a, b = 25) {
+    return a * b;
+}
 
-// const mappedPizzas = pizzas.map(function (pizza) {
-//     return pizza.name.toUpperCase();
-// });
-
-const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
-
-console.log(mappedPizzas);  // [ 'PEPPERONI' ]
-
-const pizza = {
-    name: 'Blazing Inferno',
-    getName: function() {
-        console.log(this);  // { name: 'Blazing Inferno', getName: [Function: getName] }
-        return 'Abba';
-    }
-};
-
-console.log(pizza.getName());  // Abba
-
-// const pizza = {
-//     name: 'Blazing Inferno',
-//     getName: function() {
-//         const self = this;
-//         setTimeout(function() {
-//             console.log(self);  // { name: 'Blazing Inferno', getName: [Function: getName] }
-//         }, 100);
-//     }
-// };
-
-// console.log(pizza.getName()); // undefined
-
-// const pizza = {
-//     name: 'Blazing Inferno',
-//     getName: () => pizza.name
-// };
-
-// console.log(pizza.getName());  // Blazing Inferno
+console.log(multiply(5));  // 125
+console.log(multiply(5, 35));  // 175
 

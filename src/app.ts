b18308b094948a31b334ee38edf25c616313eef5
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-21 19:16:31 +0300

    Lesson 6 - Arrow functions and implicit returns.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,41 @@
-console.log('Ahoy TypeScript!');
+const pizzas = [
+    { name: 'Pepperoni', toppings: ['pepperoni']}
+];
+
+// const mappedPizzas = pizzas.map(function (pizza) {
+//     return pizza.name.toUpperCase();
+// });
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);  // [ 'PEPPERONI' ]
+
+const pizza = {
+    name: 'Blazing Inferno',
+    getName: function() {
+        console.log(this);  // { name: 'Blazing Inferno', getName: [Function: getName] }
+        return 'Abba';
+    }
+};
+
+console.log(pizza.getName());  // Abba
+
+// const pizza = {
+//     name: 'Blazing Inferno',
+//     getName: function() {
+//         const self = this;
+//         setTimeout(function() {
+//             console.log(self);  // { name: 'Blazing Inferno', getName: [Function: getName] }
+//         }, 100);
+//     }
+// };
+
+// console.log(pizza.getName()); // undefined
+
+// const pizza = {
+//     name: 'Blazing Inferno',
+//     getName: () => pizza.name
+// };
+
+// console.log(pizza.getName());  // Blazing Inferno
+

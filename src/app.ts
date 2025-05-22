1c77aaf87040c0a3077bbd20073155b9b0e154b6
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-22 11:27:02 +0300

    Lesson 10 - Array Spread Operator.

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,11 @@
-// function sumAll(arr) {
-//     return arr.reduce((prev, next) => prev + next);
-// }
+const toppings = ['bacon', 'chilli'];
 
-// const sum = sumAll([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+const newToppings = ['pepperoni'];
 
-function sumAll(message, ...arr) {
-    console.log(arguments);  // { 
-                             //  '0': 'Hello!',
-                             //  '1': 1,
-                             //  '2': 2,
-                             //  '3': 3,
-                             //  '4': 4,
-                             //  '5': 5,
-                             //  '6': 6,
-                             //  '7': 7,
-                             //  '8': 8,
-                             //  '9': 9,
-                             //  '10': 10
-                             // }
-    console.log(arguments[0]);  // Hello!
-    console.log(message);       // Hello!
-    return arr.reduce((prev, next) => prev + next);
-}
+// const allToppings = [...toppings, ...newToppings];
 
-const sum = sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// console.log(allToppings);  // [ 'bacon', 'chilli', 'pepperoni' ]
 
-console.log(sum);  // 55
+const allToppings = [...newToppings, ...toppings];
+
+console.log(allToppings);  // [ 'pepperoni', 'bacon', 'chilli' ]

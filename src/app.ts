a1daeb255010c788b2f8ed0e55aff56d585a1f17
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-22 13:29:57 +0300

    Lesson 12 - Number Types, arguments and functions.

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,9 @@
-const pizza = {
-    name: 'Pepperoni',
-    toppings: ['pepperoni']
-};
+const pizzaCost: number = 10;
+const pizzaToppings: number = 2;
 
-// function order({ name, toppings }) {
-//     console.log(name, toppings);  // Pepperoni [ 'pepperoni' ]
-// }
-
-// order(pizza);
-
-// function order({ name: pizzaName, toppings: pizzaToppings }) {
-//     console.log(pizzaName, pizzaToppings);  // Pepperoni [ 'pepperoni' ]
-// }
-
-// order(pizza);
-
-function order({ name: pizzaName, toppings: pizzaToppings }) {
-    return { pizzaName, pizzaToppings }
+function calculatePrice(cost: number, toppings: number): number {
+    return cost + 1.5 * toppings;
 }
 
-const { pizzaName } = order(pizza);
-
-const toppings = ['pepperoni', 'bacon', 'chilli'];
-
-// const firstItem = toppings[0];
-
-const [ first, second, third ] = toppings;
-
-console.log(first, second, third);  // pepperoni bacon chilli
-
-function logToppings([first, second, third]: any) {
-    console.log(first, second, third);  // pepperoni bacon chilli
-}
+const cost: number = calculatePrice(pizzaCost, pizzaToppings);
+console.log(`Pizza costs: ${cost}`); // Pizza costs: 13

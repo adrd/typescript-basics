5faa0f6a604886382ddcea45267fb9dee559426f
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-26 15:01:04 +0300

    Lesson 24 - Object Types.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-let sumOrder: (price: number, quantity?: number) => number;
+let pizza: { name: string, price: number, getName(): string } = {
+    name: 'Plain Old Pepperoni',
+    price: 20,
+    getName() {
+        return pizza.name;
+    }
+};
 
-sumOrder = (x, y = 1) => x * y;
-
-const sum = sumOrder(25);
-
-console.log(`Total sum: ${sum}`);  // Total sum: 25
+console.log(pizza.getName());  // Plain Old Pepperoni

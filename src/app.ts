136c24f0c9a32c170600dc70c5e2403ee274de02
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-26 20:35:19 +0300

    Lesson 32 - Interfaces with Function Types.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
 interface Pizza {
-    name: string,
-    sizes: string[]
+    name: string;
+    sizes: string[];
+    getAvailableSizes(): string[];
 };
 
-// interface Pizzas {
-//     data: Pizza[]
-// }
+// type getAvailableSizes = () => void;
 
 let pizza: Pizza;
 
 function createPizza(name: string, sizes: string[]): Pizza {
     return {
         name,
-        sizes
+        sizes,
+        getAvailableSizes() {
+            return this.sizes
+        }
     }
 }
 

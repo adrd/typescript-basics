1e2e17b1e6305976e35120d2b5f9ae3f24d8a7e4
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-05-27 12:05:58 +0300

    Lesson 38 - Readonly Members.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Pizza {
     public toppings: string[] = [];
 
-    constructor(private name: string) {}
+    constructor(readonly name: string) {}
 
     public addTopping(topping: string) {
         this.toppings.push(topping);
@@ -12,4 +12,4 @@ const pizza = new Pizza('pepperoni');
 
 pizza.addTopping('pepperoni');
 
-console.log(pizza);    // Pizza { toppings: [ 'pepperoni' ], name: 'pepperoni' }
+console.log(pizza.name);  // pepperoni
